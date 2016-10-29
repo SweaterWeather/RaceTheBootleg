9d359d54be4f916da034a18ce6c8384ad41374fe
RukiYakutoshi16 <aznmayham@gmail.com>
2016-10-29 17:37:28 -0400

    Update

---
+++ Assets/LloydScript/Lloyd_ObstacleMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lloyd_ObstacleMovement : MonoBehaviour {
+    public float radius = 1;
+
+    public Vector3 speed = new Vector3(0,0,10);
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position -= speed*Time.deltaTime;
+	
+	}
+}
---
+++ Assets/LloydScript/Lloyd_Spawning_Obstacle.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Lloyd_Spawning_Obstacle : MonoBehaviour {
+
+    List<Lloyd_ObstacleMovement> obstacles;
+    public GameObject player;
+    public float xDisFromPlayer = 1;
+    public Lloyd_ObstacleMovement obs;
+
+    public int spawnLimit = 10;
+
+    public float startZoneZ = 20;
+    public float deadZoneZ = -10;
+	// Use this for initialization
+	void Start () {
+        obstacles = new List<Lloyd_ObstacleMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (obstacles.Count < 1)
+        {
+              Lloyd_ObstacleMovement obj = (Lloyd_ObstacleMovement)Instantiate(obs, getLocation(), Quaternion.identity);
+            obstacles.Add(obj);
+
+        }
+        else if (obstacles.Count < spawnLimit)
+        {
+            Lloyd_ObstacleMovement obj = (Lloyd_ObstacleMovement)Instantiate(obs, getLocation(), Quaternion.identity);
+            if (checkSurrounding(obj)) obstacles.Add(obj);
+            else { Destroy(obj.gameObject); };
+        }
+
+        removeObstaclePassZ();
+
+	}
+
+
+    void spawningObs()
+    {
+
+    }
+
+    bool checkSurrounding(Lloyd_ObstacleMovement obs)
+    {
+        Vector3 displacement;
+        for (int i = (obstacles.Count - 1); i >= 0; i--)
+        {
+            displacement = obstacles[i].transform.position - obs.gameObject.transform.position;
+            float totalRad = obstacles[i].radius + obs.radius;
+           // print(i + " Radius " + totalRad + " displacementMag " +displacement.sqrMagnitude);
+            if(displacement.sqrMagnitude > totalRad*totalRad)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    Vector3 getLocation()
+    {
+        Vector3 location;
+        float x = Random.Range(player.transform.position.x - xDisFromPlayer, player.transform.position.x + xDisFromPlayer);
+        float y = 0;
+        float z = startZoneZ;
+        location = new Vector3(x, y, z);
+
+        return location;
+    }
+
+    void removeObstaclePassZ()
+    {
+        for (int i = (obstacles.Count -1); i >= 0; i--)
+        {
+            
+            if (obstacles[i].transform.position.z < deadZoneZ)
+            {
+                Destroy(obstacles[i].gameObject);
+                obstacles.Remove(obstacles[i]);
+                print("killed");
+            }
+        }
+
+    }
+
+    
+
+
+
+}
