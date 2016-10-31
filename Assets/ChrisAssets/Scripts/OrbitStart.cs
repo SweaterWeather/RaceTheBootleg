706dec5956f8aea3f566b99dd56242b87b842054
Chris <lawsonc7@mail.lcc.edu>
2016-10-30 20:44:09 -0400

    Today I may have accidentally medeled with the main scene, sorry about that if I did.  Anyway, so I added a special assets folder just for me to work in, hopefully this helps when the time comes to merge.  I also filled it with textures and started constructing the interactive interface for the main menu.  I will finish this later this week and hopefully get working on the in-game UI soon after.

---
+++ Assets/ChrisAssets/Scripts/OrbitStart.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script animates the opening planet swinging around the sun and slowing to a stop in front of the camera.
+/// </summary>
+public class OrbitStart : MonoBehaviour {
+
+    /// <summary>
+    /// This marks the rate at which the planet will orbit.
+    /// </summary>
+    public Vector3 rotationAmmount;
+
+    /// <summary>
+    /// This is the point that the planet's orbit will stop at.
+    /// </summary>
+    public Vector3 stopPosition;
+
+    /// <summary>
+    /// This is the point the planet will start at.
+    /// </summary>
+    public Vector3 startPosition;
+
+	/// <summary>
+    /// This is the startup function, it simply sets the stop position equal to the current position.
+    /// </summary>
+	void Start () {
+	//stopPosition = 
+	}
+	
+	/// <summary>
+    /// 
+    /// </summary>
+	void Update () {
+	
+	}
+}
---
+++ Assets/ChrisAssets/Scripts/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script causes the attached game object to rotate by the rotationAmmount automatically.
+/// </summary>
+public class Rotate : MonoBehaviour {
+
+	/// <summary>
+    /// This is the ammount that the object will rotate by every frame.
+    /// </summary>
+	public Vector3 rotationAmmount;
+	
+	/// <summary>
+    /// This is the update loop, it will rotate the object according to rotationAmmount;
+    /// </summary>
+	void Update () {
+        transform.Rotate(rotationAmmount * Time.deltaTime);
+	}
+}
