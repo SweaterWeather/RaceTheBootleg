872d69d2ed9421b95b87fb05be6afaca7e21c38a
Chris <lawsonc7@mail.lcc.edu>
2016-11-07 20:45:51 -0500

    Today I started work on animating the main menu.

---
+++ Assets/Popout3.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script controls the movement of popout bars for the main menu.
+/// </summary>
+public class Popout3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
