a63082b5da4b4f27cd6a2bc11f588b6bb804e1b7
Chris <lawsonc7@mail.lcc.edu>
2016-11-09 22:35:14 -0500

    Today was a good work day, I fininshed the main menu completely and did a great ammount of visual polish for it.  The only UI element I can think of that still needs to be done is the game over, and that will be easy compared to this shit.

---
+++ Assets/ChrisAssets/Scripts/MainMenu.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This script is the primary controller for the main menu.  It has various functions that trigger in unison with the button presses.
+/// </summary>
+public class MainMenu : MonoBehaviour {
+
+    /// <summary>
+    /// This animator is the one that makes the camera swing from one menu to the next.
+    /// </summary>
+    public Animator swingTime;
+
+    /// <summary>
+    /// This is the rotation of the main menu's ring.
+    /// </summary>
+    public Animator rotation;
+
+    /// <summary>
+    /// The event system this part of the UI is currently running off of.
+    /// </summary>
+    public EventSystem eventSyst;
+
+    /// <summary>
+    /// This button represents the button on the very first start menu.
+    /// </summary>
+    public Button pressedStart;
+
+    /// <summary>
+    /// This button represents the easy button on the main menu.
+    /// </summary>
+    public Button startEasy;
+
+    /// <summary>
+    /// This button represents the medium button on the main menu.
+    /// </summary>
+    public Button startMid;
+
+    /// <summary>
+    /// This button represents the hard button on the main menu.
+    /// </summary>
+    public Button startHard;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void Update()
+    {
+        if(eventSyst.currentSelectedGameObject == startEasy.gameObject)
+        {
+            rotation.SetInteger("index", 1);
+        }
+        else if(eventSyst.currentSelectedGameObject == startMid.gameObject)
+        {
+            rotation.SetInteger("index", 2);
+        }
+        else if(eventSyst.currentSelectedGameObject == startHard.gameObject)
+        {
+            rotation.SetInteger("index", 3);
+        }
+    }
+
+	/// <summary>
+    /// 
+    /// </summary>
+	public void PressStart () {
+        swingTime.SetBool("zooming", true);
+        eventSyst.SetSelectedGameObject(startEasy.gameObject);
+        pressedStart.enabled = false;
+    }
+
+    // Update is called once per frame
+    public void PressEasy()
+    {
+        print("started easy!");
+    }
+
+    // Update is called once per frame
+    public void PressMid()
+    {
+        print("started mid!");
+    }
+
+    // Update is called once per frame
+    public void PressHard()
+    {
+        print("started hard!");
+    }
+}
---
+++ Assets/ChrisAssets/Scripts/RotateAround.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+/// This script makes the main menu planets orbit around the sun.
+/// </summary>
+public class RotateAround : MonoBehaviour {
+
+    /// <summary>
+    /// This is the degree by which the planet will rotate.
+    /// </summary>
+    float rotationAmount;
+
+	/// <summary>
+    /// This is the start function, it sets random rotational values for all components.  It also skews them slightly.
+    /// </summary>
+	void Start () {
+        rotationAmount = Random.Range(-360, 360f);
+        transform.eulerAngles = new Vector3(Random.Range(-5, 5f), 0, Random.Range(-5, 5f));
+	}
+	
+	/// <summary>
+    /// This is the update loop, it actually rotates the stupid planets.
+    /// </summary>
+	void Update () {
+        transform.localEulerAngles += new Vector3(0, rotationAmount * Time.deltaTime /2, 0);
+	}
+}
