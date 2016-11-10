3cc65d683548962b22f10830ae67634450b38516
Allen C. Gardei <gardeia@ferris.edu>
2016-11-09 20:46:29 -0500

    Hover Physics
    I have a system that will do its best to correct the players verticle
    position to the ground based on object below it using ray casts.
    Velocity can sometimes snap to 0. Will be tweaking further.

---
+++ Assets/hoverPhysics.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hoverPhysics : MonoBehaviour {
+    new Vector3 _lPos;
+    float vertVel;
+    float maxVel = .25f;
+
+    bool grounded = false;
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        RaycastHit hit;
+        _lPos = transform.position + new Vector3(0, 0, .2f);
+        Ray ray = new Ray(_lPos, Vector3.down);
+
+        if (Physics.Raycast(ray, out hit, 1.25f))
+        {
+            //player is close to ground 
+            
+                
+            if (hit.distance < 1.23) vertVel -= .003f;
+            else vertVel = 0;
+
+        }
+        else
+        {
+            //player is FLYING GUYS.
+
+            vertVel += .002f;
+            if (vertVel > maxVel) vertVel = maxVel;
+        }
+
+
+
+        //slowly lower back to floor           
+
+
+        vertVel *= .99f;
+        if (Mathf.Abs(vertVel) < .0001f) vertVel = 0;
+        transform.position += new Vector3(0, -vertVel, 0);
+
+        Debug.DrawLine(_lPos, _lPos - new Vector3(0, 1.25f, 0), Color.red);
+	}
+}
