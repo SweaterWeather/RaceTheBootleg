83cc8aa435e27ed08f44666002f001dcffef388f
Allen C. Gardei <gardeia@ferris.edu>
2016-11-14 21:25:00 -0500

    Prepping to import obstacles
    -Imported plane model from Floyd.
    -Adjusted Camera for new model
    -Created script for the floor to slide left and right with player to
    create illusion of infinite strafe.
    -emptied level 1 map to prepare for spawning obstacles.

---
+++ Assets/Allen/followPlayerX.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followPlayerX : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(player.transform.position.x, 0, 1200);
+	}
+}
